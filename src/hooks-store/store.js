4e92ff23df2978cfb985c0c838227d7f441156a0
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-11-23 00:32:56 -0300

    .

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -1,9 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 let globalState = {};
+
 let listeners = [];
+
 let actions = {};
 
 const useStore = () => {
-    
+    const setState = useState(globalState)[1];
+
+    const dispatch = actionIdentifier => {
+        
+    };
+
+    useEffect(() => {
+        listeners.push(setState);
+
+        return () => {
+            listeners = listeners.filter(li => li !== setState);
+        }
+    }, [setState]);
 };
